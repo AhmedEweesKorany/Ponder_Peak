2d70e96fe5c74b62d56c6b2063b340aaa25019e7
Ahmed Ewees Korany <ahmedewaisvi34@gmail.com>
2024-08-29 09:33:29 +0300

    update api url

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import axios from 'axios'
 
   
 const queryClient = new QueryClient()
-axios.defaults.baseURL = 'https://blogappbackend-api.up.railway.app/';
+axios.defaults.baseURL = 'https://blogappbackend-api.up.railway.app';
 ReactDOM.createRoot(document.getElementById('root')).render(
 
     <BrowserRouter>
